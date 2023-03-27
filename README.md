6087f5d205c32ebe77239e49fae3ba50610ee7c4
ritafomkina <ritakisil@MacBook-Air-Margarita.local>
2023-03-27 19:56:53 +0300

    feat: edit readme.md

---
+++ README.md
@@ -0,0 +1,2 @@
+repo for RS School Angular tasks
+first task: https://github.com/rolling-scopes-school/tasks/blob/master/tasks/angular/intro.md
---
+++ YouTube-client-app/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { SearchItemComponent } from './components/main/search-item/search-item.component';
+import { SortingAreaComponent } from './components/header/sorting-area/sorting-area.component';
+import { SearchingAreaComponent } from './components/header/searching-area/searching-area.component';
+import { LogAreaComponent } from './components/header/log-area/log-area.component';
+import { SearchResultListComponent } from './components/main/search-result-list/search-result-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    SearchItemComponent,
+    SortingAreaComponent,
+    SearchingAreaComponent,
+    LogAreaComponent,
+    SearchResultListComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
